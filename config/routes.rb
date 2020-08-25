68f28f82b6c2b0ceeda727a9f36e309b32fe0301
uziienaoto <otoaneiizu@gmail.com>
2020-08-25 18:13:27 +0900

    コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
+  resources :ramen_shops, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_051852) do
+ActiveRecord::Schema.define(version: 2020_08_25_084122) do
+
+  create_table "ramen_shops", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
